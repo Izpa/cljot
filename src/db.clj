caa25f466a955040071bff8ef4e275b43d8661a9
izpa <ispius.prime@gmail.com>
2024-09-13 02:00:22 +0300

    add db

--- src/db.clj
+++ src/db.clj
@@ -1,14 +1,30 @@
 (ns db
   (:require
+   [honey.sql :as sql]
    [integrant.core :as ig]
-   [next.jdbc :as jdbc]))
+   [migratus.core :as migratus]
+   [next.jdbc :as jdbc]
+   [next.jdbc.result-set :as rs]))
 
-(defmethod ig/init-key ::ds [_ {:keys [dbname user password-secret port]}]
-  (jdbc/get-datasource {:dbtype "postgres"
-                        :dbname dbname
-                        :user user
-                        :password password-secret
-                        :port port}))
+(defmethod ig/init-key ::ds [_ db-config]
+  (let [migratus-config {:sotre                :database
+                         :migration-dir        "migrations/"
+                         :migration-table-name "migrations"
+                         :db                   db-config}]
+    (migratus/init migratus-config)
+    (migratus/migrate migratus-config))
+  (jdbc/get-datasource db-config))
+
+(defn execute-sql-map!
+  ([ds sql-map] (execute-sql-map! sql-map ds false))
+  ([ds sql-map one?]
+   ((if one?
+      jdbc/execute-one!
+      jdbc/execute!) ds
+                     (sql/format sql-map)
+                     {:builder-fn rs/as-unqualified-kebab-maps
+                      :return-keys true
+                      :pretty true})))
 
 (defmethod ig/init-key ::execute! [_ {:keys [ds]}]
-  (partial jdbc/execute! ds))
+  (partial execute-sql-map! ds))
--- src/quiz.clj
+++ src/quiz.clj
@@ -1,1 +1,16 @@
-(ns quiz)
+(ns quiz
+  (:require
+   [integrant.core :as ig]
+   [taoensso.timbre :as log]
+   [telegrambot-lib.core :as tbot]
+   [utils :refer [pformat]]))
+
+(defmethod ig/init-key ::msg->answer [_ {:keys [bot]}]
+  (fn [msg]
+    (let [{{:keys [id]} :chat
+           :keys [data]} msg
+          command-id (cond
+                       data (keyword data)
+                       :else :default)]
+      (log/info (pformat msg))
+      (tbot/send-message bot id (pformat msg)))))
